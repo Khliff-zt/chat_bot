8e9a448ba2911b77bae81591e2a01d5ee1e114ca
Cleven <clevengodsontech@gmail.com>
2025-08-09 02:53:55 +0300

    Making Functional CLI Chat App

---
+++ src/cli_chat.py
@@ -0,0 +1,32 @@
+# cli_chat.py
+import openai
+import os
+
+# API key conf
+client = openai.OpenAI(api_key = os.getenv("OPENAI_API_KEY","openai-api-key"))
+
+def ask_ai(question):
+    """ Function that ask ai qn"""
+    try:
+        response = openai.chat.completions.create(
+            model = "gpt-4o-mini", # AI model to use
+            messages = [
+                {"role" : "system","content" : "Ask only Simple question"},
+                {"role" : "user", "content" : question }
+            ]
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        return f"Error : {e}"
+    
+# Test Functions
+if __name__ == "__main__":
+    while True:
+        user_question = input("ME : ")
+
+        if user_question.lower()=='quit':
+            break
+
+        answer = ask_ai(user_question)
+
+        print(f"AI: {answer}")
